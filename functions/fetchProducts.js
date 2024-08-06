e0f099aded92328d4021c893d111fcbea7ad37ec
ONáufrago <91100766+ONaufrago@users.noreply.github.com>
2024-08-06 02:20:41 -0300

    5

--- functions/fetchProducts.js
+++ functions/fetchProducts.js
@@ -1,19 +1,18 @@
-// functions/fetchProducts.js
-const fetch = require("node-fetch");
-
-exports.handler = async (event, context) => {
+exports.handler = async function(event, context) {
   try {
-    const response = await fetch('https://sheets.googleapis.com/v4/spreadsheets/19OfPl4OBd7QOYq2AV_qFU0ddUBvUP9jkg9IbnI8Y8w0/values/A1:H?key=AIzaSyDsXyIrF1dW6cgbD2SSfJItPa2Lngv0_XQ');
-    const data = await response.json();
+      const { default: fetch } = await import('node-fetch');
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data.values),
-    };
+      const response = await fetch('https://sheets.googleapis.com/v4/spreadsheets/19OfPl4OBd7QOYq2AV_qFU0ddUBvUP9jkg9IbnI8Y8w0/values/A1:H?key=AIzaSyDsXyIrF1dW6cgbD2SSfJItPa2Lngv0_XQ');
+      const data = await response.json();
+
+      return {
+          statusCode: 200,
+          body: JSON.stringify(data)
+      };
   } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Erro ao buscar dados." }),
-    };
+      return {
+          statusCode: 500,
+          body: JSON.stringify({ error: 'Internal Server Error' })
+      };
   }
 };
