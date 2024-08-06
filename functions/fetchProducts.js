7452978166f6029c82cd4496ef8cb13c7a47b3f8
ONáufrago <91100766+ONaufrago@users.noreply.github.com>
2024-08-06 02:02:28 -0300

    2

--- functions/fetchProducts.js
+++ functions/fetchProducts.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 
 exports.handler = async (event, context) => {
   try {
-    const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/YOUR_SPREADSHEET_ID/values/A1:H?key=${process.env.YOUR_API_KEY}`);
+    const response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/19OfPl4OBd7QOYq2AV_qFU0ddUBvUP9jkg9IbnI8Y8w0/values/A1:H?key=AIzaSyDsXyIrF1dW6cgbD2SSfJItPa2Lngv0_XQ");
     const data = await response.json();
 
     return {
